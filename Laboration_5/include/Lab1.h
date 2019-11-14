c578fee44d1c0b1355e055fefefb11b6f2fa5eac
William Elfversson <welfversson@gmail.com>
2019-11-14 23:15:36 +0100

    Restructure folders 2

--- Laboration_5/include/Lab1.h
+++
@@ -1,9 +0,0 @@
-//------------------------------------------------------------------------------
-// Lab5.h Objektbaserad programmering i C++
-// This header file is included for structural reasons and may be removed!
-//------------------------------------------------------------------------------
-
-#pragma once
-#include <string>
-
-std::string getAssignmentInfo() { return "Laboration 5!"; }
--- Project/include/proj.h
+++
@@ -1,9 +0,0 @@
-//------------------------------------------------------------------------------
-// proj.h Objektbaserad programmering i C++
-// This header file is included for structural reasons and may be removed!
-//------------------------------------------------------------------------------
-
-#pragma once
-#include <string>
-
-std::string getAssignmentInfo() { return "Project assignment!"; }
